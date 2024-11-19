8e27a11535c7422b63016d5118541e65f330e139
Williams5 <Williams5@DESKTOP-K0OH8CI>
2024-11-19 14:03:00 -0800

    get points2.m to be good, no longer need points3.m

--- JoanVersion/Points2.m
+++ JoanVersion/Points2.m
@@ -7,13 +7,15 @@ function [in,A] = Points2(qc,n,m,X,Y)
 
 P1 = qc+[m;n];
 P2 = qc+[m;-n];
-h1 = X == X(P1(1));
-h2 = Y == Y(:,P1(2));
+P11 = round((P1+4)*12.5);
+P22 = round((P2+4)*12.5);
+h1 = X == X(P11(1),:);
+h2 = Y == Y(:,P11(2));
 H1 = h1 & h2 ~= 0;
 
-h1 = X == X(P2(1));
-h2 = Y == Y(:,P2(2));
+h1 = X == X(P22(1),:);
+h2 = Y == Y(:,P22(2));
 H2 = h1 & h2 ~= 0;
 in = H1+H2;
-A = [P1;P2];
+A = [P1,P2];
 end
