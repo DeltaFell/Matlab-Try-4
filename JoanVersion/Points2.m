33c14ca46290c12a58adce5449fd419220b1c2f9
Williams5 <Williams5@DESKTOP-K0OH8CI>
2024-11-19 11:38:43 -0800

    finish up points2.m

--- JoanVersion/Points2.m
+++ JoanVersion/Points2.m
@@ -1,8 +1,19 @@
 function [in,A] = Points2(qc,n,m,X,Y)
-xi = qc(1);
-yi = qc(2);
+% xi = qc(1);
+% yi = qc(2);
+% 
+% xo = xi+n;
+% yo = yi+m;
 
-xo = xi+n;
-yo = yi+m;
+P1 = qc+[m,n];
+P2 = qc+[m,-n];
+h1 = X == X(P1(1));
+h2 = Y == Y(:,P1(2));
+H1 = h1 & h2 ~= 0;
 
+h1 = X == X(P2(1));
+h2 = Y == Y(:,P2(2));
+H2 = h1 & h2 ~= 0;
+in = H1+H2;
+A = [P1;P2];
 end
