04fc81a58de23ae4aec565f0ac8cebe06c8ae4f8
Williams5 <Williams5@DESKTOP-K0OH8CI>
2024-12-09 12:30:22 -0800

    adding changes without the messed up too big LES TSR dude

--- JoanVersion/Points2.m
+++ JoanVersion/Points2.m
@@ -1,12 +1,15 @@
 function [in,A] = Points2(qc,n,m,X,Y)
+% qc - quarterchord point
+% n - spanwise distance from quarterchord
+% m - streamwise distance from quarterchord
 % xi = qc(1);
 % yi = qc(2);
 % 
 % xo = xi+n;
 % yo = yi+m;
 
-P1 = qc+[m;n];
-P2 = qc+[m;-n];
+P1 = qc-[m;n];
+P2 = qc-[m;-n];
 P11 = round((P1+4)*12.5);
 P22 = round((P2+4)*12.5);
 h1 = X == X(P11(1),:);
